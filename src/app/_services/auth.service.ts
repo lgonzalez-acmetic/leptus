2eed01b42a54ff12fa8e535b37d5e0bfd6ac8e82
Rolando HR <rolando167@hotmail.com>
2021-11-17 16:39:55 -0500

    auth service

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8000/api/usuarios/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +14,31 @@ import { HttpClient } from "@angular/common/http";
 export class AuthService {
 
   constructor(private http: HttpClient) {}
+
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'login', {
+      username,
+      password
+    }, httpOptions);
+  }
+
+  register(name: string, email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      name,
+      email,
+      password
+    }, httpOptions);
+  }
+
+  // ----------
+
+  getUsers() {
+    // this.http.get("https://reqres.in/api/users?page=2").subscribe(data => {
+    this.http.get(AUTH_API + "lista").subscribe(data => {
+      console.log(data);
+    });
+  }
 
   messages: string[] = [];
 
@@ -17,13 +49,6 @@ export class AuthService {
 
   clear() {
     this.messages = [];
-  }
-
-  getUsers() {
-    // this.http.get("https://reqres.in/api/users?page=2").subscribe(data => {
-    this.http.get("http://localhost:8000/api/usuarios/lista").subscribe(data => {
-      console.log(data);
-    });
   }
 
 }
