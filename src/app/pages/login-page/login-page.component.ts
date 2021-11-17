fefead2cb8e42f4b361a63e405d828415a54a298
Rolando HR <rolando167@hotmail.com>
2021-11-17 17:43:54 -0500

    login 99%

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -7,19 +7,43 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-  usuario = {
-    email: '',
-    password: ''
-  }
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
 
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
     this.authService.getUsers();
   }
+  onSubmit(): void {
+    const { username, password } = this.form;
+    console.log(this.form);
 
-  login(){
-    console.log(this.usuario);
+    // this.authService.login(username, password).subscribe(
+    //   data => {
+    //     this.tokenStorage.saveToken(data.accessToken);
+    //     this.tokenStorage.saveUser(data);
 
+    //     this.isLoginFailed = false;
+    //     this.isLoggedIn = true;
+    //     this.roles = this.tokenStorage.getUser().roles;
+    //     this.reloadPage();
+    //   },
+    //   err => {
+    //     this.errorMessage = err.error.message;
+    //     this.isLoginFailed = true;
+    //   }
+    // );
   }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+
 }
