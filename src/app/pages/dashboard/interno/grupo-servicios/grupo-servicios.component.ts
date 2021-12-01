7bf325da41d760e2ba95cb25ced82ad59247e0ad
Rolando HR <rolando167@hotmail.com>
2021-12-01 14:39:55 -0500

    cambios

---
+++ src/app/pages/dashboard/interno/grupo-servicios/grupo-servicios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grupo-servicios',
+  templateUrl: './grupo-servicios.component.html',
+  styleUrls: ['./grupo-servicios.component.css']
+})
+export class GrupoServiciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/errors/page-mantenimiento/page-mantenimiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-mantenimiento',
+  templateUrl: './page-mantenimiento.component.html',
+  styleUrls: ['./page-mantenimiento.component.css']
+})
+export class PageMantenimientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
