42d20916247b077f73a72faea52f4d44aaadba05
Rolando HR <rolando167@hotmail.com>
2021-12-01 11:07:08 -0500

    cambios

---
+++ src/app/pages/home/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PricesComponent } from './prices/prices.component';
+import { TeamComponent } from './team/team.component';
+import { ContentComponent } from './content/content.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    PricesComponent,
+    TeamComponent,
+    ContentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
